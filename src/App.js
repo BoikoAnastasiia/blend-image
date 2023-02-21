45a5a412935c1ffef0b615b63feda5276305d50c
Nastya Boiko <boiko.nastasiia@gmail.com>
2023-02-21 16:47:52 +0100

    alpha doesn't work

--- src/App.js
+++ src/App.js
@@ -1,89 +1,79 @@
 import { fabric } from 'fabric';
-import { useEffect, useState } from 'react';
-import player from './player.jpeg';
-import texture from './texture.png';
+import { useEffect, useRef, useState } from 'react';
+import playerImage from './player.jpeg';
+import textureImage from './texture.png';
 import './App.css';
 
 function App() {
   const [sliderValue, setSlide] = useState(0.5);
-
-  const handleChange = (e) => {
-    console.log(e.target.value);
-    setSlide(e.target.value);
-  };
-
-  const handleBlur = (e) => {
-    const { value, name } = e.target;
-    if (e.target.value == '') {
-      setSlide(name);
-    }
-  };
+  const [fabricCanvas, setFabricCanvas] = useState(null);
+  const texture = useRef();
+  const player = useRef();
+  useEffect(() => {
+    setFabricCanvas(initFabric());
+  }, []);
 
   useEffect(() => {
-    const canvas = new fabric.Canvas('blend', {
+    if (fabricCanvas) {
+      fabricCanvas.add(
+        new fabric.Image(player.current, { scaleX: 0.2, scaleY: 0.2 }),
+        new fabric.Image(texture.current, {
+          scaleX: 0.2,
+          scaleY: 0.2,
+          left: 400,
+          opacity: 0.2,
+        })
+      );
+    }
+  }, [fabricCanvas]);
+
+  const initFabric = () => {
+    return new fabric.Canvas('blend', {
       backgroundColor: 'white',
       width: 2000,
       height: 1000,
     });
+  };
 
-    let mainImage;
+  const handleChange = (e) => {
+    setSlide(+e.target.value);
+  };
 
-    fabric.Image.fromURL(
-      player,
-      function (img) {
-        img.set({ scaleX: 0.2, scaleY: 0.2 });
-        mainImage = img;
-        canvas.add(img);
-      },
-      { crossOrigin: 'annonymous' }
-    );
+  const addFilter = () => {
+    let fabricTexture = new fabric.Image(texture.current, { opacity: 0.2 });
+    fabricCanvas._objects[0].filters = [];
+    const filter = new fabric.Image.filters.BlendImage({
+      image: fabricTexture,
+      mode: 'multiply',
+      alpha: sliderValue,
+    });
 
-    let secondImgObject;
-    fabric.Image.fromURL(
-      texture,
-      function (img) {
-        img.set({ width: 566, scaleX: 0.9, scaleY: 0.9, left: 400 });
-        secondImgObject = img;
-        canvas.add(img);
-      },
-      { crossOrigin: 'annonymous' }
-    );
+    fabricCanvas._objects[0].filters.push(filter);
+    fabricCanvas._objects[0].applyFilters();
+    fabricCanvas.requestRenderAll();
+  };
 
-    function addFilter() {
-      const filter = new fabric.Image.filters.BlendImage({
-        image: secondImgObject,
-        mode: 'multiply',
-        alpha: sliderValue,
-      });
-
-      mainImage.filters.push(filter);
-      mainImage.applyFilters();
-      canvas.requestRenderAll();
-    }
-    function resetFilter() {
-      mainImage.filters = [];
-      mainImage.applyFilters();
-      canvas.requestRenderAll();
-    }
-
-    document.getElementById('apply').addEventListener('click', addFilter);
-    document.getElementById('reset').addEventListener('click', resetFilter);
-    console.log(object);
-  }, [sliderValue]);
+  function resetFilter() {
+    fabricCanvas._objects[0].filters = [];
+    fabricCanvas._objects[0].applyFilters();
+    fabricCanvas.requestRenderAll();
+  }
 
   return (
     <div className="App">
-      <button id="apply">Apply Blend</button>
-      <button id="reset">Reset</button>
-      <input
+      <button onClick={addFilter}>Apply Blend</button>
+      <button onClick={resetFilter}>Reset</button>
+      {/* <input
         type="range"
         min="0"
         max="1"
         step="0.1"
         onChange={handleChange}
-        onBlur={handleBlur}
-      />
+        value={sliderValue}
+      /> */}
       <canvas className="canvas" id="blend"></canvas>
+      <img src={playerImage} alt="asd" ref={player} />
+      <img src={textureImage} alt="asd" ref={texture} />
     </div>
   );
 }
